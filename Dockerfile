eb6e9d2a503660d6fe2f01acad1e3116924ff2c6
Krisztian Horvath <krisztian.horvath@sequenceiq.com>
2014-10-26 14:44:26 +0100

    CLOUD-239 use the oauth based cloudbreak

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,10 @@ FROM dockerfile/java
 MAINTAINER SequenceIQ
 
 # install the cloudbreak app
-ADD https://s3-eu-west-1.amazonaws.com/seq-repo/releases/com/sequenceiq/cloudbreak/0.1-20140814121912/cloudbreak-0.1-20140814121912.jar /cloudbreak.jar
+ADD https://s3-eu-west-1.amazonaws.com/maven.sequenceiq.com/releases/com/sequenceiq/cloudbreak/0.2.9/cloudbreak-0.2.9.jar /cloudbreak.jar
 
 # install the cloudbreak-shell app
-ADD https://s3-eu-west-1.amazonaws.com/maven.sequenceiq.com/snapshots/com/sequenceiq/cloudbreak/cloudbreak-shell/0.1-SNAPSHOT/cloudbreak-shell-0.1-20140814.082233-62.jar /cloudbreak-shell.jar
+ADD https://s3-eu-west-1.amazonaws.com/maven.sequenceiq.com/releases/com/sequenceiq/cloudbreak-shell/0.2.8/cloudbreak-shell-0.2.8.jar /cloudbreak-shell.jar
 
 # Install starter script for the Cloudbreak application
 ADD add/start_cloudbreak_app.sh /
@@ -18,6 +18,7 @@ ADD add/start_cloudbreak_shell_app.sh /
 ADD add/ngrok.zip /ngrok.zip
 
 # Install zip
+RUN apt-get update
 RUN apt-get install zip
 
 RUN sudo unzip /ngrok -d /bin
--- start_cloudbreak.sh
+++ start_cloudbreak.sh
@@ -21,7 +21,7 @@ check-docker-version() {
 
 =============================================
 = ERROR                                     =
-= You are using a too old version of Docker =
+= You are using an old version of Docker =
 = Please upgrade it to at least 1.1.1       =
 =============================================
 EOF
@@ -31,21 +31,24 @@ EOF
 
 check-docker-version
 
-: ${CB_AZURE_IMAGE_URI:="http://vmdepoteastus.blob.core.windows.net/linux-community-store/community-62091-a59dcdc1-d82d-4e76-9094-27b8c018a4a1-5.vhd"}
+: ${CB_AZURE_IMAGE_URI:="http://vmdepotneurope.blob.core.windows.net/linux-community-store/community-62091-c0713e8c-bc6d-44cc-a751-bf9c35603340-5.vhd"}
 : ${CB_MANAGEMENT_CONTEXT_PATH:="/"}
 : ${CB_BLUEPRINT_DEFAULTS:="lambda-architecture,multi-node-hdfs-yarn,single-node-hdfs-yarn"}
 : ${CB_SNS_SSL:="false"}
 : ${CB_DB_ENV_DB:="cloudbreak"}
 : ${CB_HBM2DDL_STRATEGY:="create"}
-: ${CB_API_HOST:=cloudbreak.kom}
 : ${CB_API_PORT:=8080}
-: ${CB_UI_PORT:=80}
-: ${CB_API_URL:="http://$CB_API_HOST:$CB_API_PORT"}
-: ${CB_UI_ADDR:="http://$CB_API_HOST"}
 : ${CB_DB_ENV_USER:=cloudbreak}
 : ${CB_DB_ENV_PASS:=cloudbreak}
-
-: ${DOCKER_IMAGE_TAG:=0.1-hotfix}
+: ${CB_CLIENT_ID:=cloudbreak}
+: ${CB_CLIENT_SECRET:=cloudbreaksecret}
+: ${CB_SMTP_SENDER_PORT=587}
+: ${CB_SMTP_SENDER_FROM=no-reply@sequenceiq.com}
+: ${ULU_OAUTH_REDIRECT_URI=http://localhost:3000/authorize}
+: ${ULU_OAUTH_CLIENT_SECRET=uluwatusecret}
+: ${ULU_OAUTH_CLIENT_ID=uluwatu}
+: ${DOCKER_IMAGE_TAG:=latest}
+: ${ULU_DOCKER_IMAGE_TAG:=latest}
 
 if [[ "$DOCKER_IMAGE_TAG" != "latest" ]] ; then
   docker pull sequenceiq/uluwatu:$DOCKER_IMAGE_TAG
@@ -73,14 +76,29 @@ timeout=10
 echo "Wait $timeout seconds for the POSTGRES DB to start up"
 sleep $timeout
 
+docker inspect uaadb &>/dev/null && docker rm -f uaadb
+
+docker run -d --name="uaadb"  -e USER="uaaadmin" -e DB="uaa" -e PASS="uaaadmin" paintedfox/postgresql
+echo "Wait $timeout seconds for the UAA DB to start up"
+sleep $timeout
+
+docker inspect uaa &>/dev/null && docker rm -f uaa
+
+docker run -d --name="uaa" --link uaadb:db sequenceiq/uaa:1.8.1
+timeout=20
+echo "Wait $timeout seconds for the UAA to start up"
+sleep $timeout
+
 # Removes previous containers
 docker inspect cloudbreak &>/dev/null &&  docker rm -f cloudbreak
+
+UAA_ADDR=$(docker inspect -f '{{.NetworkSettings.IPAddress}}' uaa)
 
 # Start the CLoudbreak application docker container
 docker run -d --name="cloudbreak" \
 -e "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" \
 -e "AWS_SECRET_KEY=$AWS_SECRET_KEY" \
--e "CB_HBM2DDL_STRATEGY=create" \
+-e "CB_HBM2DDL_STRATEGY=$CB_HBM2DDL_STRATEGY" \
 -e "CB_SMTP_SENDER_USERNAME=$CB_SMTP_SENDER_USERNAME" \
 -e "CB_SMTP_SENDER_PASSWORD=$CB_SMTP_SENDER_PASSWORD" \
 -e "CB_SMTP_SENDER_HOST=$CB_SMTP_SENDER_HOST" \
@@ -90,15 +108,11 @@ docker run -d --name="cloudbreak" \
 -e "CB_BLUEPRINT_DEFAULTS=$CB_BLUEPRINT_DEFAULTS" \
 -e "CB_SNS_SSL=$CB_SNS_SSL" \
 -e "CB_MANAGEMENT_CONTEXT_PATH=$CB_MANAGEMENT_CONTEXT_PATH" \
--e "CB_UI_ADDR=$CB_UI_ADDR" \
--e "CB_DEFAULT_USER_EMAIL=$CB_DEFAULT_USER_EMAIL" \
--e "CB_DEFAULT_USER_PASSWORD=$CB_DEFAULT_USER_PASSWORD" \
--e "CB_DEFAULT_USER_FIRSTNAME=$CB_DEFAULT_USER_FIRSTNAME" \
--e "CB_DEFAULT_USER_LASTNAME=$CB_DEFAULT_USER_LASTNAME" \
--e "CB_DEFAULT_COMPANY_NAME=$CB_DEFAULT_COMPANY_NAME" \
+-e "CB_CLIENT_ID=$CB_CLIENT_ID" \
+-e "CB_CLIENT_SECRET=$CB_CLIENT_SECRET" \
+-e "CB_IDENTITY_SERVER_URL=http://$UAA_ADDR:8080" \
 --link postgresql:cb_db \
 -p $CB_API_PORT:8080 \
--p $CB_UI_PORT:80 \
 sequenceiq/cloudbreak:$DOCKER_IMAGE_TAG bash
 
 # we are starting the wait_for_cloudbreak_api.sh script in a container
@@ -109,31 +123,32 @@ docker run -it --rm \
   --entrypoint /bin/bash \
   sequenceiq/cloudbreak:$DOCKER_IMAGE_TAG -c /wait_for_cloudbreak_api.sh
 
-
 # Removes previous containers
 docker inspect uluwatu &>/dev/null && docker rm -f uluwatu
 
-docker run -d --name uluwatu \
-  -e CB_API_URL="$CB_API_URL" \
-  --net=container:cloudbreak \
-  sequenceiq/uluwatu:$DOCKER_IMAGE_TAG
+CB_ADDR=$(docker inspect -f "{{.NetworkSettings.IPAddress}}" cloudbreak)
+
+docker run -d --name="uluwatu" \
+-e "ULU_CLOUDBREAK_ADDRESS=http://$CB_ADDR:8080" \
+-e "ULU_IDENTITY_ADDRESS=http://$UAA_ADDR:8080" \
+-e "ULU_OAUTH_CLIENT_ID=$ULU_OAUTH_CLIENT_ID" \
+-e "ULU_OAUTH_CLIENT_SECRET=$ULU_OAUTH_CLIENT_SECRET" \
+-e "ULU_OAUTH_REDIRECT_URI=$ULU_OAUTH_REDIRECT_URI" \
+-e "ULU_SULTANS_ADDRESS=http://$UAA_ADDR:8080" \
+-p 3000:3000 sequenceiq/uluwatu:$ULU_DOCKER_IMAGE_TAG
 
 command_exists() {
 	command -v "$@" > /dev/null 2>&1
 }
 
-if command_exists boot2docker; then
-  CLOUDBREAK_IP=$(boot2docker ip 2> /dev/null)
-else
-  CLOUDBREAK_IP=$(docker inspect -f "{{.NetworkSettings.IPAddress}}" cloudbreak)
-fi
+CLOUDBREAK_IP=$(docker inspect -f "{{.NetworkSettings.IPAddress}}" cloudbreak)
+ULUWATU_IP=$(docker inspect -f "{{.NetworkSettings.IPAddress}}" uluwatu)
 
 cat <<EOF
-=============================
-= Please put this line into =
-= /etc/hosts                =
-=============================
-
-$CLOUDBREAK_IP  $CB_API_HOST
-
+=============================================
+Cloudbreak is running on: $CLOUDBREAK_IP:$CB_API_PORT
+Uluwatu is running on: localhost:3000
+        username: admin@sequenceiq.com
+        password: admin
+=============================================
 EOF
