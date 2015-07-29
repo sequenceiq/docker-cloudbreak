806d52915de4e01abf55a329a55b5a766058f340
Tamas Bihari <tamas.bihari@sequenceiq.com>
2015-07-29 14:40:52 +0200

    CLOUD-41671 in case of using urandom force to use /dev/urandom

--- bootstrap/start_cloudbreak_app.sh
+++ bootstrap/start_cloudbreak_app.sh
@@ -5,7 +5,7 @@
 echo "Starting the Cloudbreak application..."
 
 if [ "$SECURE_RANDOM" == "false" ]; then
-  CB_PARAMS=-Djava.security.egd=file:/dev/urandom
+  CB_PARAMS=-Djava.security.egd=file:/dev/./urandom
 fi
 
 java $CB_PARAMS -jar /cloudbreak.jar
