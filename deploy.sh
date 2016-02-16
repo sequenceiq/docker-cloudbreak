d934777b4a99c642b8b33de1a7521b3c6f14b3aa
lalyos <lalyos@yahoo.com>
2016-02-16 21:08:11 +0100

    Add logic to get latest version form maven metadata

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,37 @@
-: ${DOCKERHUB_USERNAME:?"required!"}
-: ${DOCKERHUB_PASSWORD:?"required!"}
+#!/bin/bash
+
+set -eo pipefail
+if [[ "$TRACE" ]]; then
+    : ${START_TIME:=$(date +%s)}
+    export START_TIME
+    export PS4='+ [TRACE $BASH_SOURCE:$LINENO][ellapsed: $(( $(date +%s) -  $START_TIME ))] '
+    set -x
+fi
+
+debug() {
+  [[ "$DEBUG" ]] && echo "-----> $*" 1>&2 || :
+}
+
 
 install_deps() {
   if ! dockerhub-tag --version &>/dev/null ;then
-    echo "---> installing dockerhub-tag binary to /usr/local/bin" 1>&2
+    debug "installing dockerhub-tag binary to /usr/local/bin"
     curl -L https://github.com/progrium/dockerhub-tag/releases/download/v0.2.0/dockerhub-tag_0.2.0_Darwin_x86_64.tgz | tar -xz -C /usr/local/bin/
   else
-    echo "---> dockerhub-tag already installed" 1>&2
-  fi
+    debug "dockerhub-tag already installed"
+fi
+}
+
+get_latest_maven_version() {
+  curl -sL  maven.sequenceiq.com/releases/com/sequenceiq/cloudbreak/maven-metadata.xml \
+      |sed -n '/<version>/ h; $ {x;s/ *<.\?version>//gp;}'
 }
 
 new_version() {
   install_deps
-  declare NEW_VERSION=${1:? version required}
+  declare NEW_VERSION=${1:-$(get_latest_maven_version)}
+
+  debug "building docker image for version: $NEW_VERSION"
 
   sed -i "/^ENV VERSION/ s/VERSION .*/VERSION ${NEW_VERSION}/" Dockerfile
 
@@ -24,6 +43,10 @@ new_version() {
 }
 
 main() {
+  : ${DOCKERHUB_USERNAME:?"required!"}
+  : ${DOCKERHUB_PASSWORD:?"required!"}
+  : ${DEBUG:=1}
+
   new_version "$@"
 }
 
